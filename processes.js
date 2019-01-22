a1fd4b2b3f0374fce034d69f64bf75dca3438d12
PureGero <puregero@gmail.com>
2019-01-22 21:44:59 +1000

    Add pm2 functionality

--- processes.js
+++ processes.js
@@ -1,8 +1,25 @@
+// UNUSED - USING PM2 TO MANAGE THE PROCESSES
+
 var fs = require('fs');
+var spawn = require('child_process').spawn;
 
 var user_data = 'user_data';
 var processes = user_data + '/processes';
+var CONSOLE_LINE_COUNT = 256;
+var RESTART_TIMEOUT = 1; // seconds
 
+// Mandatory keys {
+//      name,
+//      binPath,
+//      user,
+//      workingDirectory,
+// Optional keys
+//      preTerminateCommand,
+// Unsaved keys
+//      console,
+//      process,
+//      binIndex
+// }
 var PROCESSES = [];
 
 function startProcess(process) {
@@ -15,12 +32,27 @@ function loadProcess(data) {
     startProcess(process);
 }
 
+function getCpuTime(pid, callback) {
+    fs.readFile("/proc/" + pid + "/stat", (err, data) => {
+        if (err) {
+            callback(0); // Probably on a unix system that doesn't do /proc
+        } else {
+            var elems = data.toString().split(' ');
+            var utime = parseInt(elems[13]);
+            var stime = parseInt(elems[14]);
+            callback(utime + stime);
+        }
+    });
+}
+
+// --- Load and start all processes --- //
+
 fs.readdir(processes, (err, files) => {
     if (err) {
         console.log(err);
     } else {
         files.forEach(file => {
-            fs.readFile(processes + '/' + file, function(err, data) {
+            fs.readFile(processes + '/' + file, (err, data) => {
                 if (err) {
                     console.log(err);
                 } else {
--- server.js
+++ server.js
@@ -3,9 +3,26 @@ var https = require('https');
 var fs = require('fs');
 var url = require('url');
 var querystring = require('querystring');
-var processes = require('./processes');
 var cert = require('./cert');
 var login = require('./login');
+var pm2 = require('pm2');
+const WebSocket = require('ws');
+
+pm2.connect(function(err) {
+    if (err) {
+        console.error(err);
+        process.exit(2);
+    }
+    
+    pm2.launchBus((err, bus) => {
+        bus.on('log:out', data => {
+            appendLog(data.process.pm_id, data.data.replace('\n',''));
+        });
+        bus.on('log:err', data => {
+            appendLog(data.process.pm_id, data.data.replace('\n',''));
+        });
+    });
+});
 
 var MIMES = {
     'html': 'text/html; charset=utf-8',
@@ -19,8 +36,39 @@ var MIMES = {
 
 var SERVERS = [];
 var PAGES = {};
+var LOG = {};
+var INFO_LISTENERS = [];
+
+function appendLog(id, log) {
+    id = id.toString();
+    if (!(id in LOG))
+        LOG[id] = [];
+    LOG[id].push(log);
+    if (LOG[id].length > 300) {
+        LOG[id].splice(0, 50);
+    }
+    
+    for (var i in INFO_LISTENERS) {
+        if (INFO_LISTENERS[i].id == id) {
+            INFO_LISTENERS[i].ws.send(log);
+        }
+    }
+}
+
+function parseQueryString(str) {
+    var objURL = {};
+
+    str.replace(
+        new RegExp( "([^?=&]+)(=([^&]*))?", "g" ),
+        function( $0, $1, $2, $3 ){
+            objURL[ $1 ] = $3;
+        }
+    );
+    return objURL;
+};
 
 function requestHandler(req, res) {
+    console.log(req.url);
     // All pathnames in lowercase
     var page = url.parse(req.url).pathname.toLowerCase();
     var username = login.getUsernameFromCookies(req);
@@ -32,10 +80,51 @@ function requestHandler(req, res) {
     } else if (!username && (page == '/' || page == '/login')) {
         // Not logged in
         loginPage(req, res);
+    } else if (!username && page.substr(1) in PAGES) {
+        // Page exists, but not logged in
+        res.writeHead(303, {'Location': '/', 'Set-Cookie': 'sessid=; expires=Thu, 01 Jan 1970 00:00:00 GMT'});
+        res.end();
     } else {
         res.writeHead(404);
         res.end('404 Not Found');
     }
+}
+
+function websocketHandler(ws, req) {
+    console.log('ws://::' + req.url);
+    var page = url.parse(req.url).pathname.toLowerCase();
+    var username = login.getUsernameFromCookies(req);
+    if (username) {
+        if (page == '/info') {
+            var process = parseQueryString(url.parse(req.url).search)['id'];
+            INFO_LISTENERS.push({id: process, ws: ws});
+            ws.on('close', () => {
+                INFO_LISTENERS.splice(INFO_LISTENERS.indexOf(ws), 1);
+            });
+        }
+    } else {
+        res.writeHead(403);
+        res.end('403');
+    }
+}
+
+function readPost(req, res, callback) {
+    var queryData = '';
+        
+    req.on('data', function(data) {
+        queryData += data;
+        if(queryData.length > 1e6) { // Too much data
+            queryData = '';
+            res.writeHead(413);
+            res.end();
+            req.connection.destroy();
+        }
+    });
+
+    req.on('end', function() {
+        var post = querystring.parse(queryData);
+        callback(post);
+    });
 }
 
 function doLogin(req, res, username, password) {
@@ -59,20 +148,7 @@ PAGES.index = indexPage;
 
 function loginPage(req, res) {
     if (req.method == 'POST') {
-        var queryData = '';
-        
-        req.on('data', function(data) {
-            queryData += data;
-            if(queryData.length > 1e6) { // Too much data
-                queryData = '';
-                res.writeHead(413);
-                res.end();
-                req.connection.destroy();
-            }
-        });
-
-        req.on('end', function() {
-            var post = querystring.parse(queryData);
+        readPost(req, res, (post) => {
             doLogin(req, res, post.username, post.password);
         });
     } else {
@@ -80,6 +156,72 @@ function loginPage(req, res) {
     }
 }
 PAGES.login = loginPage;
+
+function infoPage(req, res) {
+    sendFile('info.html', req, res);
+}
+PAGES.info = infoPage;
+
+function listProcesses(req, res) {
+	pm2.list((err, processDescriptionList) => {
+	    res.writeHead(200);
+	    var o = [];
+	    for (var i in processDescriptionList) {
+	        o[i] = {};
+	        o[i].name = processDescriptionList[i].name;
+	        o[i].monit = processDescriptionList[i].monit;
+	        o[i].pm_id = processDescriptionList[i].pm_id;
+	        o[i].running = processDescriptionList[i].pid;
+	    }
+	    res.end(JSON.stringify(o));
+	});
+}
+PAGES.list = listProcesses;
+
+function processInfo(req, res) {
+    var process = parseQueryString(url.parse(req.url).search)['id'];
+	pm2.describe(process, (err, processDescription) => {
+	    if (err) {
+	        res.writeHead(500);
+	        res.end("Beep beep I'm a broken sheep");
+	        return;
+	    }
+	    res.writeHead(200);
+	    var o = {};
+	    processDescription = processDescription[0];
+	    o.name = processDescription.name;
+        o.monit = processDescription.monit;
+        o.pm_id = processDescription.pm_id;
+        o.running = processDescription.pid;
+        o.log = LOG[process];
+	    res.end(JSON.stringify(o));
+	});
+}
+PAGES.processinfo = processInfo;
+
+function stopProcess(req, res) {
+    var process = parseQueryString(url.parse(req.url).search)['id'];
+    appendLog(process, 'Received stop command');
+	pm2.sendLineToStdin(process, 'stop', (err, res) => {});
+	pm2.sendLineToStdin(process, 'end', (err, res) => {});
+	pm2.sendLineToStdin(process, 'quit', (err, res) => {});
+	pm2.stop(process, (err) => {});
+    res.writeHead(200);
+    res.end('OK');
+}
+PAGES.stop = stopProcess;
+
+function restartProcess(req, res) {
+    var process = parseQueryString(url.parse(req.url).search)['id'];
+    appendLog(process, 'Received restart command');
+	pm2.sendLineToStdin(process, 'stop', (err, res) => {});
+	pm2.sendLineToStdin(process, 'end', (err, res) => {});
+	pm2.sendLineToStdin(process, 'quit', (err, res) => {});
+	pm2.restart(process, (err) => {});
+    res.writeHead(200);
+    res.end('OK');
+}
+PAGES.restart = restartProcess;
 
 function sendFile(file, req, res) {
     var extension = file.substr(file.lastIndexOf('.') + 1);
@@ -98,6 +240,12 @@ function listenHttps(port) {
     var server = https.createServer(cert.options, requestHandler);
     server.listen(port);
     SERVERS.push(server);
+    
+    var ws = new WebSocket.Server({server});
+    ws.on('connection', (socket, req) => {
+        websocketHandler(socket, req);
+    });
+    
     console.log('Listening on https port', port);
 }
 
